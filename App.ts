6799f06b51b69f4852d246be5bb59df1a8140daa
devusman7 <109468416+devusman7@users.noreply.github.com>
2022-10-23 20:49:27 +0500

    First commit

---
+++ App.ts
@@ -0,0 +1,73 @@
+//If-else exercise
+// 1. Write a ts program to find maximum between two numbers?
+// var userInput1:string | null = prompt("Enter first Number");
+// console.log("First Number =",userInput1);
+// var userInput2:string | null = prompt("Enter Second Number");
+// console.log("Second Number =",userInput2);
+// if (Number(userInput1)>Number(userInput2)){
+//     console.log("First Number is greater than Second Number");
+// }
+// else if (Number(userInput1)<Number(userInput2)){
+//     console.log("Second Number is greater than First Number");
+// }
+// else {
+//     console.log("Numbers are equal");
+// }
+// 2. Write a ts program to find maximum between three numbers?
+// var userInput1:string | null = prompt("Enter first Number");
+// console.log("First Number =" , userInput1);
+// var userInput2:string | null = prompt("Enter Second Number");
+// console.log("Second Number =" , userInput2);
+// var userInput3:string | null = prompt("Enter Third Number");
+// console.log("Third Number =" , userInput3);
+// if ( Number(userInput1) > Number(userInput2) && Number(userInput3)){
+//     console.log("First Number is greater than Second Number and Third Number");
+// }
+// else if ( Number(userInput2) > Number(userInput1) && Number(userInput3)){
+//     console.log("Second Number is greater than First Number and Third Number");
+// }
+// else if ( Number(userInput3) > Number(userInput2) && Number(userInput1)){
+//     console.log("Third Number is greater than First Number and Second number");
+// }
+// else {
+//     console.log("Numbers are equal");
+// }
+// 3. Write a ts program to check whether a number is negative, positive or zero.
+// var Num:string | null = prompt("Enter Any Number");
+// if (Number(Num) > 0){
+//     console.log("Number is Positive")
+// }
+// else if (Number(Num) < 0){
+//     console.log("Number is Negative")
+// }
+// else if (Number(Num) == 0) {
+//     console.log("Number is Zero")
+// }
+// else {
+//     console.log("Not a defined Number")
+// }
+//  4.Write a ts program to check whether a number is divisible by 5 and 11 or not.
+// var Num:string | null = prompt("Enter Any Number");
+// if (Number(Num) % 5 == 0 && Number(Num) % 11 == 0){
+//     console.log("Number is divisible by 5 and 11")
+// }
+// else {
+//     console.log("Number is not divisible by 5 and 11")
+// }
+//  5. Write a ts program to check whether a number is even or odd.
+// var Num:string | null = prompt("Enter Any Number");
+// if (Number(Num) % 2 == 0 ){
+//     console.log("Number is Even.")
+// }
+// else{
+//     console.log("Number is Odd.")
+// }
+//  6. Write a ts program to check whether a year is leap year or not
+// var Num:string | null = prompt("Enter Any Number");
+// if (Number(Num) % 4 == 0 ){
+//     console.log("This is a Leap Year.")
+// }
+// else{
+//     console.log("This is not a Leap Year.")
+// }
+//  7. Write a ts program to check whether a character is alphabet or not
